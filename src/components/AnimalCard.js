a1eb34c9c509aafa67354c44175b4002454eecdf
Kelsey Oshiro <kelseymosh@gmail.com>
2020-11-30 16:54:02 -0700

    added image animations

--- src/components/AnimalCard.js
+++ src/components/AnimalCard.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
 export default function AnimalCard({animal}) {
+    const styleObj = {
+        width:"100px", 
+        height:"90px",
+        marginTop:"150px",
+        fontSize: "150px",
+        color: "#e00",
+        animation: `beat ${60/animal.heartrate}s infinite alternate`,
+        transformOrigin: "center",
+    }
+    
     return (
-        <div>
+    <section>
+        <div className="animal-information">
             <h2>{animal.name}</h2>
             <img src={animal.image}/>
             <h3>{animal.heartrate} beats per minute</h3>
         </div>
+        <div class="heart" style={styleObj}>
+            &#x2665;
+        </div>
+    </section>
     )
 }
